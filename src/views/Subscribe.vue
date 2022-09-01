1bdb63d74ed5b52c56547fc5c71b914ebc234a89
Charles Reverchon-Billot <charles.reverchon.billot@gmail.com>
2022-09-01 12:56:50 +0200

    début de mise en page du formulaire d'inscription

--- src/views/Subscribe.vue
+++ src/views/Subscribe.vue
@@ -3,105 +3,127 @@
     <div class="menu__button"><i class="fas fa-bars"></i></div>
     <div class="menu__title">Cartissimo.fr</div>
   </nav>
+  <section class="header">
+    <div class="header__text">
+      <h1>Référencez-vous !</h1>
+    </div>
+  </section>
   <section class="form">
     <div>
-      <p>Référencez-vous !</p>
-    </div>
-    <div>
-      <form action="" @submit.prevent="onSubmit">
-        <p>Quel sport collectionnez-vous ?</p>
+      <form action="" class="form__detail" @submit.prevent="onSubmit">
         <div>
-          <input type="checkbox" id="NBA" value="NBA" v-model="sport" />
-          <label for="NBA">NBA</label>
-
-          <input type="checkbox" id="NFL" value="NFL" v-model="sport" />
-          <label for="NFL">NFL</label>
-
-          <input type="checkbox" id="NHL" value="NHL" v-model="sport" />
-          <label for="NHL">NHL</label>
-
-          <input type="checkbox" id="MLB" value="MLB" v-model="sport" />
-          <label for="MLB">MLB</label>
-
-          <input type="checkbox" id="Soccer" value="Soccer" v-model="sport" />
-          <label for="Soccer">Soccer</label>
-
+          <p>Quel sport collectionnez-vous ?</p>
+          <div class="form__detail--sport">
+            <div>
+              <input type="checkbox" id="NBA" value="NBA" v-model="sport" />
+              <label for="NBA">NBA</label>
+            </div>
+            <div>
+              <input type="checkbox" id="NFL" value="NFL" v-model="sport" />
+              <label for="NFL">NFL</label>
+            </div>
+            <div>
+              <input type="checkbox" id="NHL" value="NHL" v-model="sport" />
+              <label for="NHL">NHL</label>
+            </div>
+            <div>
+              <input type="checkbox" id="MLB" value="MLB" v-model="sport" />
+              <label for="MLB">MLB</label>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                id="Soccer"
+                value="Soccer"
+                v-model="sport"
+              />
+              <label for="Soccer">Soccer</label>
+            </div>
+          </div>
           <!-- <input type="checkbox" id="other" value="other" />
           <label for="other">Autre</label>
           <input type="text" id="otherValue" name="other" /> -->
         </div>
 
         <p>Collectionnez-vous une (des ?) équipe NBA ?</p>
-        <div v-for="teamNba in teamsNba" :key="teamNba.id">
-          <input
-            type="checkbox"
-            :value="teamNba"
-            :id="teamNba.id"
-            v-model="equipeNba"
-          />
-          <label :for="teamNba.id">{{ teamNba }}</label>
+        <div class="form__detail--team">
+          <div v-for="teamNba in teamsNba" :key="teamNba.id">
+            <input
+              type="checkbox"
+              :value="teamNba"
+              :id="teamNba.id"
+              v-model="equipeNba"
+            />
+            <label :for="teamNba.id">{{ teamNba }}</label>
+          </div>
         </div>
 
         <p>Collectionnez-vous une (des ?) équipe NFL ?</p>
-        <div v-for="teamNfl in teamsNfl" :key="teamNfl.id">
-          <input
-            type="checkbox"
-            :value="teamNfl"
-            :id="teamNfl.id"
-            v-model="equipeNfl"
-          />
-          <label :for="teamNfl.id">{{ teamNfl }}</label>
+        <div class="form__detail--team">
+          <div v-for="teamNfl in teamsNfl" :key="teamNfl.id">
+            <input
+              type="checkbox"
+              :value="teamNfl"
+              :id="teamNfl.id"
+              v-model="equipeNfl"
+            />
+            <label :for="teamNfl.id">{{ teamNfl }}</label>
+          </div>
         </div>
-
         <p>Collectionnez-vous une (des ?) équipe NHL ?</p>
-        <div v-for="teamNhl in teamsNhl" :key="teamNhl.id">
-          <input
-            type="checkbox"
-            :value="teamNhl"
-            :id="teamNhl.id"
-            v-model="equipeNhl"
-          />
-          <label :for="teamNhl.id">{{ teamNhl }}</label>
+        <div class="form__detail--team">
+          <div v-for="teamNhl in teamsNhl" :key="teamNhl.id">
+            <input
+              type="checkbox"
+              :value="teamNhl"
+              :id="teamNhl.id"
+              v-model="equipeNhl"
+            />
+            <label :for="teamNhl.id">{{ teamNhl }}</label>
+          </div>
         </div>
-
         <p>Collectionnez-vous une (des ?) équipe MLB ?</p>
-        <div v-for="teamMlb in teamsMlb" :key="teamMlb.id">
-          <input
-            type="checkbox"
-            :value="teamMlb"
-            :id="teamMlb.id"
-            v-model="equipeMlb"
-          />
-          <label :for="teamMlb.id">{{ teamMlb }}</label>
-        </div>
 
+        <div class="form__detail--team">
+          <div v-for="teamMlb in teamsMlb" :key="teamMlb.id">
+            <input
+              type="checkbox"
+              :value="teamMlb"
+              :id="teamMlb.id"
+              v-model="equipeMlb"
+            />
+            <label :for="teamMlb.id">{{ teamMlb }}</label>
+          </div>
+        </div>
         <p>Collectionnez-vous une (des ?) équipe de Soccer ?</p>
-        <div v-for="teamSoccer in teamsSoccer" :key="teamSoccer.id">
-          <input
-            type="checkbox"
-            :value="teamSoccer"
-            :id="teamSoccer.id"
-            v-model="equipeSoccer"
-          />
-          <label :for="teamSoccer.id">{{ teamSoccer }}</label>
-        </div>
 
+        <div class="form__detail--team">
+          <div v-for="teamSoccer in teamsSoccer" :key="teamSoccer.id">
+            <input
+              type="checkbox"
+              :value="teamSoccer"
+              :id="teamSoccer.id"
+              v-model="equipeSoccer"
+            />
+            <label :for="teamSoccer.id">{{ teamSoccer }}</label>
+          </div>
+        </div>
         <p>
           Collectionnez-vous un ou des joueurs en particulier ? (Indiquez le
           Prénom et le Nom, et séparez les joueurs par une virgule : Michael
           Jordan, Scottie Pippen)
         </p>
-        <input type="text" v-model="joueur" />
+        <input class="subscribe__form--input" type="text" v-model="joueur" />
 
         <p>
           Avez-vous un autre type de collection (ni joueur, ni équipe, mais sur
           d'autres thèmes...)
         </p>
-        <input type="selec" v-model="collec" />
+        <input type="selec" class="subscribe__form--input" v-model="collec" />
 
         <p>En quelle année avez-vous acheté vos premières cartes ?</p>
 
-        <select v-model="premiereCarte">
+        <select class="subscribe__form--input" v-model="premiereCarte">
           <option value="Avant 1986">Avant 1986</option>
           <option value="Entre 1986 et 1990">Entre 1986 et 1990</option>
           <option value="Entre 1991 et 1995">Entre 1991 et 1995</option>
@@ -119,7 +141,7 @@
           indiquez 2020 !)
         </p>
 
-        <select v-model="actif">
+        <select class="subscribe__form--input" v-model="actif">
           <option value="Avant 1986">Avant 1986</option>
           <option value="Entre 1986 et 1990">Entre 1986 et 1990</option>
           <option value="Entre 1991 et 1995">Entre 1991 et 1995</option>
@@ -132,7 +154,7 @@
         </select>
 
         <p>Dans quel pays habitez-vous ?</p>
-        <select v-model="pays">
+        <select class="subscribe__form--input" v-model="pays">
           <option value="France">France</option>
           <option value="Belgique">Belgique</option>
           <option value="Algérie">Algérie</option>
@@ -146,39 +168,47 @@
         </select>
 
         <p>Quel est le code postal de la ville dans laquelle vous habitez ?</p>
-        <input type="text" v-model="cp" />
+        <input class="subscribe__form--input" type="text" v-model="cp" />
 
         <p>
           Sous quel Nom ou Pseudonyme souhaitez-vous apparaître dans l'annuaire
           ?
         </p>
-        <input type="text" v-model="pseudo" />
+        <input class="subscribe__form--input" type="text" v-model="pseudo" />
         <p>
           Laissez un message court décrivant votre passion pour les cartes, vos
           recherches, etc...
         </p>
-        <input type="textarea" v-model="message" />
+        <input
+          class="subscribe__form--input"
+          type="textarea"
+          v-model="message"
+        />
         <p>
           Peut-on vous contacter via Twitter ? Si oui, précisez votre alias !
           (exemple : @panini)
         </p>
-        <input type="text" v-model="twitter" />
+        <input class="subscribe__form--input" type="text" v-model="twitter" />
         <p>
           Peut-on vous contacter via Instagram ? Si oui, précisez votre alias !
           (exemple : @panini)
         </p>
-        <input type="text" v-model="instagram" />
+        <input class="subscribe__form--input" type="text" v-model="instagram" />
         <p>
           Souhaitez-vous indiquer une autre façon de vous contacter ? (Ne
           laissez ni email, ni numéro de téléphone)
         </p>
-        <input type="text" v-model="contact" />
+        <input class="subscribe__form--input" type="text" v-model="contact" />
         <p>
           Quel est votre email ? (Il ne sera pas diffusé : il ne servira que
           pour vous envoyer le lien d'accès à l'annuaire)
         </p>
-        <input type="email" v-model="mail" />
-        <button @click="createUser()">CHERCHER</button>
+        <input class="subscribe__form--input" type="email" v-model="mail" />
+        <div>
+          <button @click="createUser()" class="header__form--button">
+            S'INSCRIRE
+          </button>
+        </div>
       </form>
     </div>
   </section>
