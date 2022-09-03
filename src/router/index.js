1c5bae4fbdb8272fa15932ebd0c075b49e4e18e6
Charles Reverchon-Billot <charles.reverchon.billot@gmail.com>
2022-09-03 10:32:19 +0200

    test3

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import ResultatsTeam from "../views/ResultatsTeam.vue";
 import Subscribe from "../views/Subscribe.vue";
 const router = createRouter({
   mode: history,
-  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
