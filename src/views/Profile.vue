d4cc055ca2a168f0a9eeebdfcbda713253451c92
Charles Reverchon-Billot <charles.reverchon.billot@gmail.com>
2022-10-12 01:08:44 +0200

    feat : édition équipe NBA profil

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -7,78 +7,132 @@
       <h1>{{ user.pseudo }}</h1>
     </div>
   </section>
-  <section class="test">Email : {{ user.mail }}</section>
-  <div class="test2">
-    <section>
-      <div>
-        <div>
-          <i class="fa fa-pen" @click="changeMode('Nba')"></i>
-          <h4>Equipe NBA :</h4>
-        </div>
-        <p v-for="team in user.NBA" :key="team.id" class="box">
+  <section class="p-2">
+    Email : {{ user.mail }}
+    <div>
+      <div
+        class="
+          flex
+          align-middle
+          gap-2
+          justify-between
+          border-solid border-b border-b-tertiaryBis
+          mb-2
+          pb-2
+          mt-3
+        "
+      >
+        <h4 class="font-bold">Equipes NBA :</h4>
+        <span v-if="mode == ''"
+          ><i class="fa fa-pen text-primary" @click="changeMode('Nba')"></i
+        ></span>
+        <span v-if="mode == 'editNba'"
+          ><i class="fa fa-xmark text-primary" @click="changeMode('close')"></i
+        ></span>
+      </div>
+      <div class="flex flex-row flex-wrap gap-2 mb-3">
+        <p
+          v-for="team in user.NBA"
+          :key="team.id"
+          class="
+            text-white
+            bg-primary
+            rounded-lg
+            flex
+            gap-2
+            pt-1
+            pb-1
+            px-2
+            py-2
+            align-middle
+            font-small
+            text-sm
+          "
+        >
           <router-link
             :to="{
               name: 'resultatsteam',
               params: { sport: 'NBA', team: `${team}` },
             }"
             >{{ team }}</router-link
-          ><i
-            class="fa-solid fa-xmark"
-            v-if="mode == 'editNba'"
-            @click="deleteTeam('NBA', team)"
-          ></i>
-        </p>
-        <div v-if="mode == 'editNba'">
-          <select v-model="selectNba" @change="addTeam('NBA', selectNba)">
-            <option
-              v-for="teamNba in teamsNba"
-              :key="teamNba.id"
-              :value="teamNba"
-            >
-              {{ teamNba }}
-            </option>
-          </select>
-          <button @click="modifyAccount()">Modifier</button>
-        </div>
-      </div>
-      <div>
-        <h4>Equipe NFL :</h4>
-        <p v-for="team in user.NFL" :key="team.id" class="box">
-          <router-link :to="{ path: `/NFL/${team}` }">{{ team }}</router-link>
+          ><span
+            ><i
+              class="fa-solid fa-xmark"
+              v-if="mode == 'editNba'"
+              @click="deleteTeam('NBA', team)"
+            ></i
+          ></span>
         </p>
       </div>
-      <div v-if="user.NHL.length">
-        <h4>Equipe NHL :</h4>
-        <p v-for="team in user.NHL" :key="team.id" class="box">
-          <router-link :to="{ path: `/NHL/${team}` }">{{ team }}</router-link>
-        </p>
+      <div v-if="mode == 'editNba'">
+        <select
+          v-model="selectNba"
+          @change="addTeam('NBA', selectNba)"
+          class="
+            block
+            h-10
+            py-2
+            px-3
+            border border-gray-300
+            bg-white
+            rounded-md
+            shadow-sm
+            text-sm
+            mb-2
+          "
+        >
+          <option value="NBA" disabled class="font-small">NBA</option>
+          <option
+            v-for="teamNba in teamsNba"
+            :key="teamNba.id"
+            :value="teamNba"
+            class="font-small"
+          >
+            {{ teamNba }}
+          </option>
+        </select>
+        <p class="text-primary font-bold" id="err"></p>
+        <button
+          @click="modifyAccount()"
+          class="bg-tertiary rounded-lg pt-1 pb-1 px-2 py-2 text-white"
+        >
+          Modifier
+        </button>
       </div>
-      <div v-if="user.MLB.length">
-        <h4>Equipe MLB :</h4>
-        <p v-for="team in user.MLB" :key="team.id" class="box">
-          <router-link :to="{ path: `/MLB/${team}` }">{{ team }}</router-link>
-        </p>
-      </div>
-      <div v-if="user.SOCCER.length">
-        <h4>Equipe Soccer :</h4>
-        <p v-for="team in user.SOCCER" :key="team.id" class="box">
-          <router-link :to="{ path: `/SOCCER/${team}` }">{{
-            team
-          }}</router-link>
-        </p>
-      </div>
-    </section>
-    <section>
-      <h4>Joueurs :</h4>
-      <p v-for="joueur in user.joueur" :key="joueur.id" class="box">
-        {{ joueur }}
+    </div>
+    <div v-if="user.NFL.length">
+      <h4>Equipe NFL :</h4>
+      <p v-for="team in user.NFL" :key="team.id" class="box">
+        <router-link :to="{ path: `/NFL/${team}` }">{{ team }}</router-link>
       </p>
-    </section>
-    <section>
-      <h4>Collection :</h4>
-      <p>{{ user.collec }}</p>
-    </section>
-  </div>
+    </div>
+    <div v-if="user.NHL.length">
+      <h4>Equipe NHL :</h4>
+      <p v-for="team in user.NHL" :key="team.id" class="box">
+        <router-link :to="{ path: `/NHL/${team}` }">{{ team }}</router-link>
+      </p>
+    </div>
+    <div v-if="user.MLB.length">
+      <h4>Equipe MLB :</h4>
+      <p v-for="team in user.MLB" :key="team.id" class="box">
+        <router-link :to="{ path: `/MLB/${team}` }">{{ team }}</router-link>
+      </p>
+    </div>
+    <div v-if="user.SOCCER.length">
+      <h4>Equipe Soccer :</h4>
+      <p v-for="team in user.SOCCER" :key="team.id" class="box">
+        <router-link :to="{ path: `/SOCCER/${team}` }">{{ team }}</router-link>
+      </p>
+    </div>
+
+    <!-- <h4>Joueurs :</h4>
+    <p v-for="joueur in user.joueur" :key="joueur.id" class="box">
+      {{ joueur }}
+    </p>
+
+    <h4>Collection :</h4>
+    <p>{{ user.collec }}</p> -->
+  </section>
 </template>
 
 <script>
@@ -104,13 +158,20 @@ export default {
       teamsNhl: [],
       teamsMlb: [],
       teamsSoccer: [],
-      selectNba: "",
+      selectNba: "NBA",
       mode: "",
     };
   },
   methods: {
     addTeam(league, selectedTeam) {
-      this.user[league].push(selectedTeam);
+      if (this.user[league].includes(selectedTeam)) {
+        document.getElementById("err").innerHTML =
+          "Vous collectionnez déjà cette équipe !";
+      } else {
+        document.getElementById("err").innerHTML = "";
+        this.user[league].push(selectedTeam);
+        this.selectNba = "NBA";
+      }
     },
 
     deleteTeam(league, selectedTeam) {
@@ -119,7 +180,11 @@ export default {
     },
 
     changeMode(league) {
-      this.mode = "edit" + league;
+      if (league == "close") {
+        this.mode = "";
+      } else {
+        this.mode = "edit" + league;
+      }
     },
 
     modifyAccount() {
