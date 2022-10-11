f9a0d901dddcfc43ceaf410ff92e9605185973f7
Charles Reverchon-Billot <charles.reverchon.billot@gmail.com>
2022-10-12 01:08:28 +0200

    feat style : ajout tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#C22C25",
+      secondary: "#8B2621",
+      tertiary: "#505050",
+      tertiaryBis: "#707070",
+      white: "#FFFFFF",
+    },
+  },
+  plugins: [],
+};
