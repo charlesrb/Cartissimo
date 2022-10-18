eab152f86bf8685151ee82a02e9522991638383d
Charles Reverchon-Billot <charles.reverchon.billot@gmail.com>
2022-10-18 10:54:50 +0200

    feat : ajout des conditions inscription / login

--- src/views/Inscription.vue
+++ src/views/Inscription.vue
@@ -4,38 +4,45 @@
   <div class="header__form" v-if="mode == 'create'">
     <div class="header__form--detail">
       <h1 class="font-bold text-lg pt-4">S'inscrire</h1>
-
-      <label for="pseudo">Votre pseudo</label>
-      <input
-        type="text"
-        class="header__form--input"
-        name="pseudo"
-        v-model="user.pseudo"
-        required
-      />
-      <label for="email">Votre email</label>
-      <input
-        type="email"
-        class="header__form--input"
-        name="email"
-        v-model="user.mail"
-        required
-      />
-      <label for="password">Votre mot de passe</label>
-      <input
-        type="password"
-        class="header__form--input"
-        name="password"
-        v-model="user.password"
-        required
-      />
-      <button
-        class="bg-secondary text-white rounded-3xl py-2 px-4 my-2"
-        type="submit"
-        @click.stop.prevent="createUser()"
-      >
-        S'inscrire
-      </button>
+      <form class="header__form--detail">
+        <label for="pseudo">Votre pseudo</label>
+        <input
+          type="text"
+          class="header__form--input"
+          name="pseudo"
+          v-model="user.pseudo"
+          required
+        />
+        <label for="email">Votre email</label>
+        <input
+          type="email"
+          class="header__form--input"
+          name="email"
+          v-model="user.mail"
+          required
+        />
+        <label for="password">Votre mot de passe</label>
+        <input
+          type="password"
+          class="header__form--input"
+          name="password"
+          v-model="user.password"
+          required
+        />
+        <div class="flex flex-col justify-center align-middle">
+          <p
+            id="err"
+            class="text-primary my-2 text-sm px-4 text-center"
+          ></p>
+        </div>
+        <button
+          class="bg-secondary text-white rounded-3xl py-2 px-4 my-2"
+          type="submit"
+          @click.stop.prevent="createUser()"
+        >
+          S'inscrire
+        </button>
+      </form>
     </div>
   </div>
 
@@ -59,6 +66,12 @@
           v-model="user.password"
           required
         />
+        <div class="flex flex-col justify-center align-middle">
+          <p
+            id="err"
+            class="text-primary my-2 text-sm px-4 text-center"
+          ></p>
+        </div>
         <button
           class="bg-secondary text-white rounded-3xl py-2 px-4 my-2"
           type="submit"
@@ -120,15 +133,30 @@ export default {
     SwitchToCreateAccount: function () {
       this.mode = "create";
       this.errMessage = "";
+      document.getElementById("err").innerHTML = "";
+
     },
     SwitchToLogin: function () {
       this.mode = "login";
       this.errMessage = "";
+      document.getElementById("err").innerHTML = "";
+
     },
 
     createUser() {
       const user = { ...this.user };
+      const regexPassword = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,32})/;
+      const regexEmail = /^[\w-.]{2,32}@([\w-]+\.)+[\w-]{2,4}$/g;
 
+      document.getElementById("err").innerHTML = "";
+      if (!regexPassword.test(user.password)) {
+        document.getElementById("err").innerHTML = "Votre mot de passe doit faire entre 8 et 32 caractères + 1 minuscule min + 1 maj min + 1 caractère spécial";
+        return;
+      }
+      if (!regexEmail.test(user.mail)) {
+        document.getElementById("err").innerHTML = "Votre adresse email doit être de la forme exemple@mail.com";
+        return;
+      }
       instanceUser
         .post("/", user)
         .then((data) => {
@@ -154,9 +182,12 @@ export default {
           if (data.status === 200) {
             this.$router.push("/profile");
           }
+          
         })
         .catch((error) => {
           error;
+          console.log(error)
+          document.getElementById("err").innerHTML = error.response.data.error;          
         });
     },
   },
