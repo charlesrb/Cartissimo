dd970a4c1a21848f4dd05f9e5a11d37dc1fb442e
Charles Reverchon-Billot <charles.reverchon.billot@gmail.com>
2022-10-22 08:05:43 +0200

    feat : installation emailjs / contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,17 +1,23 @@
 <template>
   <Navigation></Navigation>
-  <div class="p-4">
+  <div v-if="mode == 'formulaire'" class="p-4 flex justify-center flex-col max-w-sm align-middle m-auto">
     <h2 class="font-bold text-2xl text-start">
       Une remarque ? <br />Contactez-nous !
     </h2>
-    <form action="" class="flex flex-col mt-10">
-        <label class="mt-4 mb-2" for="mail">Votre email</label>
+    <form ref="form" @submit.prevent="sendEmail" class="flex flex-col mt-10">
+      <label class="mt-4 mb-2" for="mail">Votre email</label>
 
       <input
         class="border border-tertiaryBis text-black h-10 rounded-3xl px-4"
         type="email"
         v-model="user.mail"
-        name="mail"
+        name="email"
+        id=""
+      />
+      <input
+        type="hidden"
+        v-model="user.pseudo"
+        name="name"
         id=""
       />
       <label class="mt-4 mb-2" for="message">Votre message</label>
@@ -19,22 +25,29 @@
       <textarea
         class="border border-tertiaryBis text-black h-60 rounded-3xl p-4"
         type="text"
-        name=""
+        
         placeholder="Votre message..."
+        v-model="message"
+        name="message"
         id=""
       ></textarea>
       <button
-        class="bg-secondary rounded-3xl h-10 pt-1 pb-1 px-4 py-2 text-white justify-self-end mt-4"
+        class="bg-secondary rounded-3xl h-10 pt-1 pb-1 px-4 py-2 text-white justify-self-end mt-4" type="submit" value="Send"
       >
         Envoyer
       </button>
     </form>
+  </div>
+  <div v-if="mode == 'envoye'" class="p-4 flex justify-center flex-col max-w-sm align-middle m-auto">
+    <h2 class="font-bold text-2xl text-start">Message envoyé</h2>
+    <p class="text-primary font-bold"><router-link to="/">Retour à l'accueil</router-link></p>
   </div>
 </template>
 
 <script>
 import Navigation from "../components/Navigation.vue";
 import axios from "axios";
+import emailjs from "@emailjs/browser";
 
 const instanceUser = axios.create({
   baseURL: import.meta.env.VITE_API_ENDPOINT + "/api/user",
@@ -46,9 +59,30 @@ export default {
   data() {
     return {
       user: {},
+      message: "",
+      mode:"formulaire",
     };
   },
-
+  methods: {
+    sendEmail() {
+      emailjs
+        .sendForm(
+          import.meta.env.VITE_SERVICE_ID,
+          import.meta.env.VITE_TEMPLATE_ID,
+          this.$refs.form,
+          import.meta.env.VITE_PUBLIC_KEY
+        )
+        .then(
+          (result) => {
+            console.log("SUCCESS!", result.text);
+            this.mode = "envoye"
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+          }
+        );
+    },
+  },
   components: { Navigation },
   mounted() {
     const userId = localStorage.getItem("userId");
@@ -61,8 +95,7 @@ export default {
   },
   beforeCreate() {
     document.title = "Cartissimo - Collectionneurs de cartes";
-
-  }
+  },
 };
 </script>
 
